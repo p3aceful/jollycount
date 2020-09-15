c72e6821961c879b6e281b886f940329ad58a6c4
Fredrik Carlsen <kptfredrik@gmail.com>
2020-09-15 20:00:16 +0200

    switch from moment to date-fns

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import moment from 'moment';
+
+import { 
+    isWithinInterval,
+    add,
+    differenceInSeconds,
+    differenceInHours,
+    differenceInDays,
+    differenceInMinutes
+} from 'date-fns';
 
 var header = document.createElement('h1');
 var span = document.createElement('span');
@@ -8,14 +16,28 @@ header.appendChild(span);
 document.body.appendChild(header);
 
 function update() {
-    var xmas = moment(new Date('December 24, 2019, 00:00:00'));
-    var now = moment(Date.now());
-    var days = xmas.diff(now, 'days');
-    var hours = xmas.subtract(days, 'days').diff(now, 'hours');
-    var minutes = xmas.subtract(hours, 'hours').diff(now, 'minutes');
-    var seconds = xmas.subtract(minutes, 'minutes').diff(now, 'seconds');
 
-    var test = `${days} ${days == 1 ? 'dag' : 'dager'} 
+    let now = new Date();
+
+    let year = now.getFullYear();
+
+    let christmas = new Date(`${year}-12-24T00:00:00Z`);
+    let newYear = new Date(`${year}-12-31T23:59:59Z`);
+
+
+    // If christmas has already been this year start countdown to next
+    let isChristmas = isWithinInterval(now, { start: christmas, end: newYear })
+
+    if (isChristmas) {
+        year = year + 1;
+    }
+
+    let days = differenceInDays(christmas, now);
+    let hours = differenceInHours(christmas, add(now, { days }));
+    let minutes = differenceInMinutes(christmas, add(now, { days, hours }));
+    let seconds = differenceInSeconds(christmas, add(now, { days, hours, minutes }));
+
+    let test = `${days} ${days == 1 ? 'dag' : 'dager'} 
     ${hours} ${hours == 1 ? 'time' : 'timer'} 
     ${minutes} ${minutes == 1 ? 'minutt' : 'minutter og'} 
     ${seconds} ${seconds == 1 ? 'sekund' : 'sekunder'} til jul!`;
@@ -25,3 +47,19 @@ function update() {
 }
 
 update();
+
+
+/**
+ * Interesting!
+ */
+function isDST() {
+    const today = new Date();
+    const jan = new Date(new Date().getFullYear(), 0, 1);
+    const jul = new Date(new Date().getFullYear(), 6, 1);
+
+    const stdTimesoneOffset = Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
+
+    return today.getTimezoneOffset() < stdTimesoneOffset;
+}
+
+
